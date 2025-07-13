99bd482dc10d7e3dd9377d8a590a3cdc5a51000d
ikram713 <i.messaoud@esi-sba.dz>
2025-07-13 15:32:24 +0200

    add home page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,9 +12,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<QueryDocumentSnapshot> data = [];
+
+  bool isLoading = true;
+
+  getData() async {
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection("categories").get();
+    data.addAll(querySnapshot.docs);
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        onPressed: () {
+          Navigator.of(context).pushNamed('addcategory');
+        },
+        child: const Icon(Icons.add, color: Colors.white, size: 30),
+      ),
       appBar: AppBar(
         title: const Text('Home Page'),
         actions: [
@@ -20,18 +47,76 @@ class _HomePageState extends State<HomePage> {
             icon: const Icon(Icons.logout),
             onPressed: () async {
               GoogleSignIn googleSignIn = GoogleSignIn();
-              if( await googleSignIn.isSignedIn()) {
-              await googleSignIn.disconnect();
-              };
+              if (await googleSignIn.isSignedIn()) {
+                await googleSignIn.disconnect();
+              }
               await FirebaseAuth.instance.signOut();
               Navigator.of(context).pushReplacementNamed('login');
             },
           ),
         ],
       ),
-      body:  Center(
-          child : Text("Welcome!")
-      ),
+      body: isLoading == true ? 
+          const Center(child: CircularProgressIndicator()) :
+          GridView.builder(
+              itemCount: data.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisExtent: 200,
+              ),
+              itemBuilder: (context, i) {
+                return InkWell(
+                  onLongPress: (){
+                    AwesomeDialog(
+                    context: context,
+                    dialogType: DialogType.warning,
+                    animType: AnimType.rightSlide,
+                    title: 'warning',
+                    desc: 'are u sure u wanna delete this category?',
+                    btnCancelOnPress: () {
+                      print("cancel pressed");
+                    },
+                    btnOkOnPress: () async {
+                      await FirebaseFirestore.instance
+                          .collection("categories")
+                          .doc(data[i].id)
+                          .delete();
+                      data.removeAt(i);
+                      setState(() {});
+                      print("Category Deleted");
+                      AwesomeDialog(
+                        context: context,
+                        dialogType: DialogType.success,
+                        title: 'Success',
+                        desc: 'Category deleted successfully',
+                      ).show();
+                        
+                    },
+                    ).show();
+                  },
+                  child: Card(
+                    elevation: 4,
+                    margin: const EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(15),
+                          child: Image.asset("images/folder.png", height: 100),
+                        ),
+                        Text(
+                          data[i]['name'],
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
